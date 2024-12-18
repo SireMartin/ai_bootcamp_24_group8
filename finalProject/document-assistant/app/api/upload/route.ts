6b21e9b6edfbdec5ba7f11713f2dfb1b1190a434
Maarten Deheegher <made@skylux.be>
2024-12-18 11:55:01 +0100

    extract more data from documents

--- finalProject/document-assistant/app/api/upload/route.ts
+++ finalProject/document-assistant/app/api/upload/route.ts
@@ -1,9 +1,12 @@
 import OpenAI, { toFile } from "openai";
+import { useState } from "react";
 import { NextResponse, NextRequest } from 'next/server';
 
 const openai = new OpenAI();
 
 export const runtime = "edge";
+
+let documentIndex: number = 0;
 
 export async function POST(req: NextRequest) {
   console.log("Received request");
@@ -14,129 +17,50 @@ export async function POST(req: NextRequest) {
   const documentAssistantName: string = "documentAssistant";
   const documentAssistantVectorStoreName: string = "documentAssistantVectorStore";
 
-  const prompt = `Analyze this receipt and extract the following information in a structured format:
-  - Merchant details (name, address, zipcode, city, country, VAT number, bank account)
-  - Invoice details (invoice data, total price, currency)
-  - Line items with their name, quantities, price and unit price
-  Please ensure the output matches exactly the specified JSON structure.`;
+  const developerInstructions = `You analyze images and extract all possible data from it. 
+    As these images are generally scans or pictures taken from paper receipts and invoices, you have to scan for typical commercial fields.
+    Just to name a few of these fields, but other typically invoice fields can be found:
+    - Merchant details (name, address, zipcode, city, country, VAT number, bank account)
+    - Buyer details (name, address, zipcode, city, country, VAT number, bank account). They come mostly if the document is an invoice and thus contains a VAT number.
+    - Invoice details (invoice date, purchasing date, total price, currency, vat rate)
+    - Line items with their name, quantities, VAT, prices (with and without vat) and unit price. These can both be products or services.
+    If you can find a VAT number on the invoice, you will find prices with and without VAT. If both types of prices are not explicitly listed, you can calculate those based on a vat percentage, listed somewhere.
+    Be aware that separate line items could be charged different amount of VAT, depending on the type of service provided.
+    If you find an entity with VAT 0847260950 then this should indicate the buyer region of the document.
+    If you can find other invoice related fields on the document, feel free to add them to the result.
+    The output you provide must be in JSON fromat.`;
 
   try {
     const response = await openai.chat.completions.create({
       model: "gpt-4o-mini",
       messages: [
         {
-          role: "user",
+          role: "developer",
           content: [
             {
               type: "text",
-              text: prompt,
-            },
+              text: developerInstructions,
+            }
+          ]
+        },
+        {
+          "role": "user",
+          content:[
             {
               type: "image_url",
               image_url: {
                 url: data,
               },
             },
+            {
+              type: "text",
+              text: "Analyse the provided image of an invoice / receipt to extract all possible commercial data from it."
+            }
           ],
         },
       ],
       response_format: {
-        type: "json_schema", json_schema: {
-          "strict": true, "name": "invoiceData", "schema": {
-            "type": "object",
-            "properties": {
-              "merchant": {
-                "type": "object",
-                "properties": {
-                  "name": {
-                    "type": "string"
-                  },
-                  "address": {
-                    "type": "string"
-                  },
-                  "zipcode": {
-                    "type": "string"
-                  },
-                  "city": {
-                    "type": "string"
-                  },
-                  "country": {
-                    "type": "string"
-                  },
-                  "vat": {
-                    "type": "string"
-                  },
-                  "bankAccount": {
-                    "type": "string"
-                  }
-                },
-                "required": [
-                  "name",
-                  "address",
-                  "zipcode",
-                  "city",
-                  "country",
-                  "vat",
-                  "bankAccount"
-                ],
-                "additionalProperties": false
-              },
-              "invoice": {
-                "type": "object",
-                "properties": {
-                  "invoiceDate": {
-                    "type": "string"
-                  },
-                  "totalPrice": {
-                    "type": "number"
-                  },
-                  "currency": {
-                    "type": "string"
-                  }
-                },
-                "required": [
-                  "invoiceDate",
-                  "totalPrice",
-                  "currency"
-                ],
-                "additionalProperties": false
-              },
-              "lineItems": {
-                "type": "array",
-                "items": {
-                  "type": "object",
-                  "properties": {
-                    "name": {
-                      "type": "string"
-                    },
-                    "quantityItems": {
-                      "type": "number"
-                    },
-                    "totalPrice": {
-                      "type": "number"
-                    },
-                    "unitPrice": {
-                      "type": "number"
-                    }
-                  },
-                  "required": [
-                    "name",
-                    "quantityItems",
-                    "totalPrice",
-                    "unitPrice",
-                  ],
-                  "additionalProperties": false
-                }
-              }
-            },
-            "required": [
-              "merchant",
-              "invoice",
-              "lineItems"
-            ],
-            "additionalProperties": false
-          }
-        }
+        type: "json_object"
       },
     });
     const result = JSON.parse(response.choices[0].message.content || "{}");
@@ -156,7 +80,9 @@ export async function POST(req: NextRequest) {
       console.log("using existing vectorstore with id " + vectorStoreId);
     }
 
-    const f = await toFile(Buffer.from(JSON.stringify(result)), `${result.invoice.invoiceDate}-${result.merchant.name}.json`);
+    //const f = await toFile(Buffer.from(JSON.stringify(result)), `${result.invoice.invoiceDate}-${result.merchant.name}.json`);
+    const f = await toFile(Buffer.from(JSON.stringify(result)), `${vectorStoreId}_${documentIndex}.json`);
+    documentIndex = documentIndex + 1;
     let fileCreateResp: {id: string} = await openai.files.create({file: f, purpose: "assistants"});
     //console.log(JSON.stringify("file id : " + fileCreateResp.id));
     openai.beta.vectorStores.files.create(vectorStoreId, {file_id: fileCreateResp.id});
@@ -167,8 +93,8 @@ export async function POST(req: NextRequest) {
       const createdAssistant = await openai.beta.assistants.create({ model: "gpt-4o-mini", 
         name: documentAssistantName,
         description: "an assistant for your invoice and receipt data", 
-        instructions: "Your vector store is filled with json data of parsed tickets and invoices. You provide answers to questions about ticket and invoice content and make calculations on the data. The json properties to use for retrieving documents, are the merchant.name and invoice.invoiceDate.",
-        tools: [{type: 'file_search'}],
+        instructions: "You analyse extracted json data of invoice an receipt documents from your vector store. You provide answers to users questions about ticket and invoice content and make calculations through your code_interpreter on the data if necessary to achieve the results.",
+        tools: [{type: 'file_search'}, {type: 'code_interpreter'}],
         tool_resources: {file_search: {vector_store_ids: [vectorStoreId]}}});
       console.log("created new assistent with id " + createdAssistant.id);
     }
